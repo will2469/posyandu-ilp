d8b8ab728bd1ea273eb62d1679e3dcfa3f199bb3
willEga <will.i.is.ega@gmail.com>
2024-08-07 19:41:46 +0700

    reload project

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+// eslint.config.mjs
+import { defineConfig } from 'eslint-define-config';
+
+export default defineConfig({
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+});
